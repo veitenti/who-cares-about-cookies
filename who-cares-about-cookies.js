1937e44a58d03f7c90c27215b5234c41057f84fd
Tim V <veitenti@posteo.de>
2020-06-11 13:55:23 +0200

    avoid wrong detection of banners by limiting their possible size

--- who-cares-about-cookies.js
+++ who-cares-about-cookies.js
@@ -1,66 +1,98 @@
-var keywords = ['cookie', 'tracking', 'gdpr', 'consent'];
+var keywords = ["cookie", "tracking", "gdpr", "consent"];
 
-function findCookieBanners() {
+function findCookieBanners(lengthLimit) {
   let banners = [];
-  keywords.forEach(keyword => {
-    banners.push(...findCookieBannersByKeyword(keyword));
+  keywords.forEach((keyword) => {
+    banners.push(...findCookieBannersByKeyword(keyword, lengthLimit));
   });
-  return banners.filter(b => b);
+  return banners.filter((b) => b);
 }
-function findCookieBannersByKeyword(keyword) {
-  return [...document.getElementsByTagName('*')]
-    .filter(candidate => matchesKeyword(candidate, keyword))
-    .map(match => findCookieBannerContainer(match))
-    .filter(container => container);
+function findCookieBannersByKeyword(keyword, lengthLimit) {
+  return [...document.getElementsByTagName("*")]
+    .filter((candidate) => matchesKeyword(candidate, keyword))
+    .map((match) => findCookieBannerContainer(match, lengthLimit))
+    .filter((container) => container);
 }
-function findCookieBannerContainer(candidate) {
+function findCookieBannerContainer(candidate, lengthLimit) {
   if (candidate) {
-    return isBannerContainer(candidate)
+    return isBannerContainer(candidate, lengthLimit)
       ? candidate
-      : findCookieBannerContainer(candidate.parentElement);
+      : findCookieBannerContainer(candidate.parentElement, lengthLimit);
   }
 }
 
 function matchesKeyword(candidate, keyword) {
-  return classMatchesKeyword(candidate, keyword) || idMatchesKeyword(candidate, keyword) || textContentMatchesKeyword(candidate, keyword);
+  return (
+    classMatchesKeyword(candidate, keyword) ||
+    idMatchesKeyword(candidate, keyword) ||
+    textContentMatchesKeyword(candidate, keyword)
+  );
 }
 function classMatchesKeyword(candidate, keyword) {
-  return candidate.className && candidate.className.toLowerCase && candidate.className.toLowerCase().includes(keyword);
+  return (
+    candidate.className &&
+    candidate.className.toLowerCase &&
+    candidate.className.toLowerCase().includes(keyword)
+  );
 }
 function idMatchesKeyword(candidate, keyword) {
-  return candidate.id && candidate.id.toLowerCase && candidate.id.toLowerCase().includes(keyword);
+  return (
+    candidate.id &&
+    candidate.id.toLowerCase &&
+    candidate.id.toLowerCase().includes(keyword)
+  );
 }
 function textContentMatchesKeyword(candidate, keyword) {
-  return candidate.innerText && candidate.innerText.toLowerCase().includes(keyword) && isKeywordLeaf(candidate, keyword);
+  return (
+    candidate.innerText &&
+    candidate.innerText.toLowerCase().includes(keyword) &&
+    isKeywordLeaf(candidate, keyword)
+  );
 }
 function isKeywordLeaf(candidate, keyword) {
-  return [...candidate.children].filter(child => child.innerText && child.innerText.includes(keyword)).length == 0;
+  return (
+    [...candidate.children].filter(
+      (child) => child.innerText && child.innerText.includes(keyword)
+    ).length == 0
+  );
 }
-function isBannerContainer(candidate) {
-  return candidate
-    && parseInt(getComputedStyle(candidate)['z-index'])
-    && (
-      getComputedStyle(candidate).position == 'fixed'
-      || getComputedStyle(candidate).position == 'relative'
-      || getComputedStyle(candidate).position == 'absolute'
-    );
+function isBannerContainer(candidate, lengthLimit) {
+  return (
+    candidate &&
+    parseInt(getComputedStyle(candidate)["z-index"]) &&
+    hasPositionStyleUsedForBanners(candidate) &&
+    candidate.innerText.length < lengthLimit
+  );
+}
+function hasPositionStyleUsedForBanners(candidate) {
+  return (
+    getComputedStyle(candidate).position == "fixed" ||
+    getComputedStyle(candidate).position == "relative" ||
+    getComputedStyle(candidate).position == "absolute"
+  );
 }
 
-function removeCookieBanners() {
-  findCookieBanners().filter(banner => banner).forEach(banner => {
-    console.log("removing cookie banner:");
-    console.log(banner);
-    banner.remove();
-  });
+function removeCookieBanners(lengthLimit) {
+  findCookieBanners(lengthLimit)
+    .filter((banner) => banner)
+    .forEach((banner) => {
+      console.log(
+        "removing cookie banner with length " +
+          banner.innerText.length +
+          " and element:"
+      );
+      console.log(banner);
+      banner.remove();
+    });
 }
 
 // immediate execution
-removeCookieBanners();
+removeCookieBanners(200);
 
 // for lazy loaded cookie consent plugins
 setTimeout(function () {
-  removeCookieBanners();
+  removeCookieBanners(200);
 }, 250);
 setTimeout(function () {
-  removeCookieBanners();
+  removeCookieBanners(1000);
 }, 3000);
